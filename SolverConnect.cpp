cd727da5e7c07f129605f83fa1f5b929dce270ac
Pedro Santos FIXO <pedro-santos@hotmail.com>
2014-05-27 22:56:34 +0100

    - Read configuration from file.

--- SolverConnect.cpp
+++ SolverConnect.cpp
@@ -2,6 +2,8 @@
 #include "stdafx.h"
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <boost/thread/thread.hpp>
 
 #include "solver.hpp"
@@ -13,39 +15,87 @@ int main(int argc, char* argv[])
 	int n_cores, n_col, n_lin, n_col2, n_lin2;
 	int i;
 	char desig;
+	bool usedFile = false;
 
-	cout << "Insira o numero de cores:" << endl;
-	cin >> n_cores;
+	ifstream configFile;
 
-	cout << "Insira o numero de linhas:" << endl;
-	cin >> n_lin;
+	if(argc != 2)
+	{
 
-	cout << "Insira o numero de colunas:" << endl;
-	cin >> n_col;
+		cout << "Insira o numero de cores:" << endl;
+		cin >> n_cores;
+
+		cout << "Insira o numero de linhas:" << endl;
+		cin >> n_lin;
+
+		cout << "Insira o numero de colunas:" << endl;
+		cin >> n_col;
+
+	} else{
+	
+		// open to read config file
+		configFile.open(argv[1]);
+
+		if(configFile.is_open())
+		{
+		
+			usedFile = true;
+
+			configFile >> n_lin;
+			configFile >> n_col;
+
+			configFile >> n_cores;
+
+		}
+	
+	}
 
 	GlobalData::setNumberPoints(n_cores);
 
 	SOLVER solver(n_cores, n_lin, n_col);
 
-	for(i = 0; i < n_cores; i++)
+	if(!usedFile)
 	{
 
-		cout << "Insira a linha1(1-n) da cor "<< i+1 << ":" << endl;
-		cin >> n_lin;
+		for(i = 0; i < n_cores; i++)
+		{
 
-		cout << "Insira a coluna1(1-n) da cor "<< i+1 << ":" << endl;
-		cin >> n_col;
+			cout << "Insira a linha1(1-n) da cor "<< i+1 << ":" << endl;
+			cin >> n_lin;
 
-		cout << "Insira a linha2(1-n) da cor "<< i+1 << ":" << endl;
-		cin >> n_lin2;
+			cout << "Insira a coluna1(1-n) da cor "<< i+1 << ":" << endl;
+			cin >> n_col;
 
-		cout << "Insira a coluna2(1-n) da cor "<< i+1 << ":" << endl;
-		cin >> n_col2;
+			cout << "Insira a linha2(1-n) da cor "<< i+1 << ":" << endl;
+			cin >> n_lin2;
 
-		cout << "Insira a designacao:" << endl;
-		cin >> desig;
+			cout << "Insira a coluna2(1-n) da cor "<< i+1 << ":" << endl;
+			cin >> n_col2;
 
-		solver.set_linhas_colunas_id(n_lin-1, n_col-1, n_lin2-1, n_col2-1,desig, i);
+			cout << "Insira a designacao:" << endl;
+			cin >> desig;
+
+			solver.set_linhas_colunas_id(n_lin-1, n_col-1, n_lin2-1, n_col2-1,desig, i);
+
+		}
+
+	} else{
+	
+		// read final definitions
+		for(i = 0; i < n_cores; i++)
+		{
+		
+			configFile >> n_lin;
+			configFile >> n_col;
+			configFile >> desig;
+
+			configFile >> n_lin2;
+			configFile >> n_col2;
+			configFile >> desig;
+
+			solver.set_linhas_colunas_id(n_lin-1, n_col-1, n_lin2-1, n_col2-1,desig, i);
+		
+		}
 
 	}
 
